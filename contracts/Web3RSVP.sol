191dc9f63643fb99e42cc270e12cb50b75022810
the-supernova <biswalabhijit04@gmail.com>
2022-09-11 23:47:45 +0530

    Add logic for confirm all attendees and send unclaimed deposits

--- contracts/Web3RSVP.sol
+++ contracts/Web3RSVP.sol
@@ -115,3 +115,52 @@ function confirmAttendee(bytes32 eventId, address attendee) public {
 
     require(sent, "Failed to send Ether");
 }
+
+function confirmAllAttendees(bytes32 eventId) external {
+    // look up event from our struct with the eventId
+    CreateEvent memory myEvent = idToEvent[eventId];
+
+    // make sure you require that msg.sender is the owner of the event
+    require(msg.sender == myEvent.eventOwner, "NOT AUTHORIZED");
+
+    // confirm each attendee in the rsvp array
+    for (uint8 i = 0; i < myEvent.confirmedRSVPs.length; i++) {
+        confirmAttendee(eventId, myEvent.confirmedRSVPs[i]);
+    }
+}
+
+function withdrawUnclaimedDeposits(bytes32 eventId) external {
+    // look up event
+    CreateEvent memory myEvent = idToEvent[eventId];
+
+    // check that the paidOut boolean still equals false AKA the money hasn't already been paid out
+    require(!myEvent.paidOut, "ALREADY PAID");
+
+    // check if it's been 7 days past myEvent.eventTimestamp
+    require(
+        block.timestamp >= (myEvent.eventTimestamp + 7 days),
+        "TOO EARLY"
+    );
+
+    // only the event owner can withdraw
+    require(msg.sender == myEvent.eventOwner, "MUST BE EVENT OWNER");
+
+    // calculate how many people didn't claim by comparing
+    uint256 unclaimed = myEvent.confirmedRSVPs.length - myEvent.claimedRSVPs.length;
+
+    uint256 payout = unclaimed * myEvent.deposit;
+
+    // mark as paid before sending to avoid reentrancy attack
+    myEvent.paidOut = true;
+
+    // send the payout to the owner
+    (bool sent, ) = msg.sender.call{value: payout}("");
+
+    // if this fails
+    if (!sent) {
+        myEvent.paidOut = false;
+    }
+
+    require(sent, "Failed to send Ether");
+
+}
