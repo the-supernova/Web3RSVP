0a92bbc635970e620cf1871d0e9453b3dc9a9dfc
the-supernova <biswalabhijit04@gmail.com>
2022-10-06 19:32:57 +0530

    Fix confirm attendees

--- frontend/pages/my-events/past/[id].js
+++ frontend/pages/my-events/past/[id].js
@@ -10,83 +10,83 @@ import Alert from "../../../components/Alert";
 import Head from "next/head";
 import DashboardNav from "../../../components/DashboardNav";
 
-const confirmAttendee = async (attendee) => {
-  try {
-    const rsvpContract = connectContract();
-
-    if (rsvpContract) {
-      const txn = await rsvpContract.confirmAttendee(event.id, attendee);
-      setLoading(true);
-      console.log("Minting...", txn.hash);
-
-      await txn.wait();
-      console.log("Minted -- ", txn.hash);
-      setSuccess(true);
-      setLoading(false);
-      setMessage("Attendance has been confirmed.");
-    } else {
-      console.log("Ethereum object doesn't exist!");
-    }
-  } catch (error) {
-    setSuccess(false);
-    // setMessage(
-    //   `Error: ${process.env.NEXT_PUBLIC_TESTNET_EXPLORER_URL}tx/${txn.hash}`
-    // );
-    setMessage("Error!");
-    setLoading(false);
-    console.log(error);
-  }
-};
-
-const confirmAllAttendees = async () => {
-  console.log("confirmAllAttendees");
-  try {
-    const rsvpContract = connectContract();
-
-    if (rsvpContract) {
-      console.log("contract exists");
-      const txn = await rsvpContract.confirmAllAttendees(event.id, {
-        gasLimit: 300000,
-      });
-      console.log("await txn");
-      setLoading(true);
-      console.log("Mining...", txn.hash);
-
-      await txn.wait();
-      console.log("Mined -- ", txn.hash);
-      setSuccess(true);
-      setLoading(false);
-      setMessage("All attendees confirmed successfully.");
-    } else {
-      console.log("Ethereum object doesn't exist!");
-    }
-  } catch (error) {
-    setSuccess(false);
-    // setMessage(
-    //   `Error: ${process.env.NEXT_PUBLIC_TESTNET_EXPLORER_URL}tx/${txn.hash}`
-    // );
-    setMessage("Error!");
-    setLoading(false);
-    console.log(error);
-  }
-};
-
-function checkIfConfirmed(event, address) {
-  for (let i = 0; i < event.confirmedAttendees.length; i++) {
-    let confirmedAddress = event.confirmedAttendees[i].attendee.id;
-    if (confirmedAddress.toLowerCase() == address.toLowerCase()) {
-      return true;
-    }
-  }
-  return false;
-}
-
 function PastEvent({ event }) {
   const { data: account } = useAccount();
   const [success, setSuccess] = useState(null);
   const [message, setMessage] = useState(null);
   const [loading, setLoading] = useState(null);
   const [mounted, setMounted] = useState(false);
+
+  const confirmAttendee = async (attendee) => {
+    try {
+      const rsvpContract = connectContract();
+
+      if (rsvpContract) {
+        const txn = await rsvpContract.confirmAttendee(event.id, attendee);
+        setLoading(true);
+        console.log("Minting...", txn.hash);
+
+        await txn.wait();
+        console.log("Minted -- ", txn.hash);
+        setSuccess(true);
+        setLoading(false);
+        setMessage("Attendance has been confirmed.");
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      setSuccess(false);
+      // setMessage(
+      //   `Error: ${process.env.NEXT_PUBLIC_TESTNET_EXPLORER_URL}tx/${txn.hash}`
+      // );
+      setMessage("Error!");
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  const confirmAllAttendees = async () => {
+    console.log("confirmAllAttendees");
+    try {
+      const rsvpContract = connectContract();
+
+      if (rsvpContract) {
+        console.log("contract exists");
+        const txn = await rsvpContract.confirmAllAttendees(event.id, {
+          gasLimit: 300000,
+        });
+        console.log("await txn");
+        setLoading(true);
+        console.log("Mining...", txn.hash);
+
+        await txn.wait();
+        console.log("Mined -- ", txn.hash);
+        setSuccess(true);
+        setLoading(false);
+        setMessage("All attendees confirmed successfully.");
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      setSuccess(false);
+      // setMessage(
+      //   `Error: ${process.env.NEXT_PUBLIC_TESTNET_EXPLORER_URL}tx/${txn.hash}`
+      // );
+      setMessage("Error!");
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  function checkIfConfirmed(event, address) {
+    for (let i = 0; i < event.confirmedAttendees.length; i++) {
+      let confirmedAddress = event.confirmedAttendees[i].attendee.id;
+      if (confirmedAddress.toLowerCase() == address.toLowerCase()) {
+        return true;
+      }
+    }
+    return false;
+  }
 
   useEffect(() => {
     setMounted(true);
